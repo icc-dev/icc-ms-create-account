825a938f004d51141155fa1ea0e3b17840387697
Ian Cardenas C <iancardenas96@gmail.com>
2022-11-29 21:19:11 -0300

    clear project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountsController } from './controllers/accounts/accounts.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, AccountsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
