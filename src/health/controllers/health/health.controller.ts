817f46a4ca79d2641059a750bba698d9cdb60130
Ian Cardenas C <iancardenas96@gmail.com>
2022-11-30 07:38:48 -0300

    add check method to health controller

--- src/health/controllers/health/health.controller.ts
+++ src/health/controllers/health/health.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheckService, HttpHealthIndicator, HealthCheck } from '@nestjs/terminus';
 
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+    constructor(
+        private health: HealthCheckService,
+        private http: HttpHealthIndicator,
+    ) {}
+
+    @Get()
+    @HealthCheck()
+    check() {
+        return this.health.check([
+            () => this.http.pingCheck('google', 'https://google.com'),
+        ])
+    }
+}
