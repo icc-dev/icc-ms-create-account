4868710eb3ea5ea1e636dd24ec9914a0a46b4685
Ian Cardenas C <iancardenas96@gmail.com>
2022-11-30 07:23:17 -0300

    add terminusModule to imports of healthModule

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 
-@Module({})
+@Module({
+    imports: [TerminusModule]
+})
 export class HealthModule {}
