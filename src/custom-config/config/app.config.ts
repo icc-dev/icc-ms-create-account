05d71dd73049206fb843cf9869629f6d726ec782
Ian Cardenas C <iancardenas96@gmail.com>
2022-12-03 20:40:25 -0300

    add(app-config): new property timezone

--- src/custom-config/config/app.config.ts
+++ src/custom-config/config/app.config.ts
@@ -2,5 +2,6 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('application', () => ({
     port: process.env.PORT || 3000,
-    environment: process.env.ENVIRONMENT || 'development'
+    environment: process.env.ENVIRONMENT || 'development',
+    timeZone: process.env.TZ || 'America/Santiago'
 }));
