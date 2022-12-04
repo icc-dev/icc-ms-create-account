3f3151023d510475b7854a011f539a9e540dd487
Ian Cardenas C <iancardenas96@gmail.com>
2022-12-03 23:54:58 -0300

    modify main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -9,11 +9,15 @@ async function bootstrap() {
   app = await NestFactory.create(
     AppModule
   );
+  app.setGlobalPrefix('icc/ms/accounts');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   const config =  app.get(ConfigService);
   const port = config.get<number>('application.port');
   await app.listen(port);
-  console.log(`Microservice already running in http://localhost:${port}/`)
+  console.log(`Microservice already running in ${await app.getUrl()}/`)
 }
 bootstrap();
 
-export default app;
+export {app};
