fbeb59dea2886f68de5ce7dd7f1efe06aafa287f
Ian Cardenas C <iancardenas96@gmail.com>
2022-12-04 11:54:06 -0300

    add custom config providers and import

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -4,25 +4,35 @@ import { Module } from '@nestjs/common';
 import { CustomConfigModule } from '../custom-config/custom-config.module';
 import { AddController } from './controllers/add/add.controller';
 import { AccountsService } from './services/accounts/accounts.service';
+import { AccountsMockService } from './services/accounts/accounts.mock.service';
 import { MongooseModule } from '@nestjs/mongoose';
+
+const configMongoose = {
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb+srv://${
+                configService.get<string>('database.username')
+            }:${
+                configService.get<string>('database.password')
+            }@${
+                configService.get<string>('database.host')
+            }/${
+                configService.get<string>('database.databasename')
+            }`
+    }),
+    inject: [ConfigService],
+}
+
+const accountServiceProvider = {
+    provide: AccountsService,
+    useClass:
+        process.env.NODE_ENV === 'testing' ?
+        AccountsMockService : AccountsService
+}
 @Module({
     imports: [
         CustomConfigModule,
-        MongooseModule.forRootAsync({
-            imports: [ConfigModule],
-            useFactory: async (configService: ConfigService) => ({
-                uri: `mongodb+srv://${
-                        configService.get<string>('database.username')
-                    }:${
-                        configService.get<string>('database.password')
-                    }@${
-                        configService.get<string>('database.host')
-                    }/${
-                        configService.get<string>('database.databasename')
-                    }`
-            }),
-            inject: [ConfigService],
-        }),
+        MongooseModule.forRootAsync(configMongoose),
         MongooseModule.forFeature([
             {
                 name: Accounts.name,
@@ -31,6 +41,6 @@ import { MongooseModule } from '@nestjs/mongoose';
         ])
     ],
     controllers: [AddController],
-    providers: [AccountsService]
+    providers: [accountServiceProvider]
 })
 export class AccountsModule {}
