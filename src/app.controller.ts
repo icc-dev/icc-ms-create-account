60ed9373ee7ea541ddf2af13ce685ddce9901e8a
Ian Cardenas C <iancardenas96@gmail.com>
2022-12-03 23:54:35 -0300

    modify app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from './services/app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): any {
+    // return this.appService.getHello();
+    return {
+      hello: this.appService.getHello(),
+      port: this.configService.get<string>('application.port'),
+      environment: this.configService.get<string>('application.environment')
+    }
   }
 }
