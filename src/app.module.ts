db3634db289fc22b2de397a8242f8f30e4774f6e
Ian Cardenas C <iancardenas96@gmail.com>
2022-12-04 22:33:36 -0300

    delete app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './services/app.service';
 import { AccountsModule } from './accounts/accounts.module';
 import { CustomConfigModule } from './custom-config/custom-config.module';
 
 @Module({
   imports: [AccountsModule, CustomConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
