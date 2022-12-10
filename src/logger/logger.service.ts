4a55ce4b943a87d6ec80067daf94a1f3f2bfbb98
Ian Cardenas C <iancardenas96@gmail.com>
2022-12-10 15:07:40 -0300

    create logger service in module

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,8 +1,44 @@
-import { Injectable, Scope, ConsoleLogger } from '@nestjs/common';
-
-@Injectable({ scope: Scope.TRANSIENT })
-export class LoggerService extends ConsoleLogger {
-    setContext(context: string) {
-        super.setContext(context);
+import { Injectable, Scope } from '@nestjs/common';
+import * as crypto from 'crypto';
+import * as winston from 'winston';
+@Injectable({ scope: Scope.REQUEST })
+export class LoggerService {
+    private _logger: winston.Logger;
+    
+    constructor() {
+        this._logger = winston.createLogger({
+            level: 'info',
+            format: winston.format.json(),
+            defaultMeta: {
+                saga: 'account',
+                service: 'create',
+                type: 'ms',
+                trackId: crypto.randomBytes(8).toString("hex"),
+            },
+            transports: [
+                new winston.transports.Console({
+                    format: winston.format.combine(
+                        winston.format.colorize(),
+                        winston.format.simple(),
+                        winston.format.metadata(),
+                        winston.format.timestamp(),
+                    ),
+                    level: 'info'
+                })
+            ]
+        })
+    }
+    
+    log(message: string, aditionalData?: any) {
+        this._logger.log('info', message, aditionalData);
+    }
+    warn(message: string, aditionalData?: any) {
+        this._logger.warn(message, aditionalData);
+    }
+    error(message: string, aditionalData?: any) {
+        this._logger.error(message, aditionalData);
+    }
+    debug(message: string, aditionalData?: any) {
+        this._logger.debug(message, aditionalData);
     }
 }
