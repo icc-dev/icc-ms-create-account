5d3148495ecd09a2a3fec54f851a5445cf8abe14
Ian Cardenas C <iancardenas96@gmail.com>
2022-12-10 06:24:26 -0300

    modify spec e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,28 @@
+import 'reflect-metadata';
+
+import { AccountsModule } from './../src/accounts/accounts.module';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      imports: [AccountsModule],
+    })
+    .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/icc/ms/accounts/v1/add (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .post('/icc/ms/accounts/v1/add')
       .expect(200)
       .expect('Hello World!');
   });
+
 });
