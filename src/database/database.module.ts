b09131a4e99e6f85b087e1f06bdf4d4a9c683889
Ian Cardenas C <iancardenas96@gmail.com>
2022-12-16 18:50:23 -0300

    remove unused provider

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,8 @@
-import { ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { databaseProviders } from './providers/database.providers';
 
 @Module({
-  providers: [...databaseProviders, ConfigService],
+  providers: [...databaseProviders],
   exports: [...databaseProviders],
 })
 export class DatabaseModule {}
