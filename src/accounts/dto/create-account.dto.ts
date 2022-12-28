1b5b0c6bbaed71ebde7e25544fd29962c98348cd
Ian Cardenas C <iancardenas96@gmail.com>
2022-12-28 09:24:25 -0300

    apply format

--- src/accounts/dto/create-account.dto.ts
+++ src/accounts/dto/create-account.dto.ts
@@ -1,31 +1,34 @@
-import { TypeAccountAvailable, StatusAccountAvailable } from "@accounts/enums/accounts.enum";
+import {
+  TypeAccountAvailable,
+  StatusAccountAvailable,
+} from '@accounts/enums/accounts.enum';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateAccountDto {
-    @ApiProperty({
-        type: String,
-        required: true,
-    })
-    email: string;
-    @ApiProperty({
-        type: String,
-        required: true,
-        enum: TypeAccountAvailable
-    })
-    accountType: TypeAccountAvailable;
-    @ApiProperty({
-        type: String,
-        required: true,
-        enum: StatusAccountAvailable
-    })
-    status: StatusAccountAvailable;
-    @ApiProperty({
-        type: String,
-        required: true
-    })
-    password: String
-    
-    static describe(instance): Array<string> {
-        return Object.getOwnPropertyNames(instance);
-    }
+  @ApiProperty({
+    type: String,
+    required: true,
+  })
+  email: string;
+  @ApiProperty({
+    type: String,
+    required: true,
+    enum: TypeAccountAvailable,
+  })
+  accountType: TypeAccountAvailable;
+  @ApiProperty({
+    type: String,
+    required: true,
+    enum: StatusAccountAvailable,
+  })
+  status: StatusAccountAvailable;
+  @ApiProperty({
+    type: String,
+    required: true,
+  })
+  password: String;
+
+  static describe(instance): Array<string> {
+    return Object.getOwnPropertyNames(instance);
+  }
 }
--- src/accounts/dto/created-account.dto.ts
+++ src/accounts/dto/created-account.dto.ts
@@ -1,22 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { StatusAccountAvailable, TypeAccountAvailable } from '@accounts/enums/accounts.enum';
+import {
+  StatusAccountAvailable,
+  TypeAccountAvailable,
+} from '@accounts/enums/accounts.enum';
 export class CreatedAccountDto {
-    @ApiProperty()
-    createdAt: Date;
-    @ApiProperty()
-    updatedAt: Date;
-    @ApiProperty()
-    lastLogin: Date;
-    @ApiProperty()
-    status: StatusAccountAvailable;
-    @ApiProperty()
-    accountType: TypeAccountAvailable;
-    @ApiProperty()
-    restrictedByLevel: Boolean;
-    @ApiProperty()
-    accountLevel: Number;
-    @ApiProperty()
-    subscripted: Boolean;
-    @ApiProperty()
-    email: String;
+  @ApiProperty()
+  createdAt: Date;
+  @ApiProperty()
+  updatedAt: Date;
+  @ApiProperty()
+  lastLogin: Date;
+  @ApiProperty()
+  status: StatusAccountAvailable;
+  @ApiProperty()
+  accountType: TypeAccountAvailable;
+  @ApiProperty()
+  restrictedByLevel: Boolean;
+  @ApiProperty()
+  accountLevel: Number;
+  @ApiProperty()
+  subscripted: Boolean;
+  @ApiProperty()
+  email: String;
 }
